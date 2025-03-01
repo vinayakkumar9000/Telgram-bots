433d9ba29dcbd175becf9aed8279adec460944e7
IND OFFICIAL <81799193+vinayakkumar9000@users.noreply.github.com>
2025-03-01 13:48:14 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,6 @@
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+MAIL_TM_API = "https://api.mail.tm"
+GUERRILLA_MAIL_API = "https://api.guerrillamail.com/ajax.php"
+
+DEFAULT_PROVIDER = "mail.tm"  # Can be "mail.tm" or "guerrilla"
