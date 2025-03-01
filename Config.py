60bfe476cd9e1d8a53a4bd22abf4493d70814c89
IND OFFICIAL <81799193+vinayakkumar9000@users.noreply.github.com>
2025-03-01 13:55:26 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,9 @@
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")  # Telegram Bot Token
 MAIL_TM_API = "https://api.mail.tm"
 GUERRILLA_MAIL_API = "https://api.guerrillamail.com/ajax.php"
-
-DEFAULT_PROVIDER = "mail.tm"  # Can be "mail.tm" or "guerrilla"
+DEFAULT_PROVIDER = "mail.tm"
